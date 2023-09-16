9b4790f66388cc7bc145fe01baabbc206b57e520
Supratim-Pathak <supratimpathak@gmail.com>
2023-09-16 21:39:18 +0530

    ==

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require("express");
 
 const app = express();
 
-var corsOptions = {
-  origin: "*"
-};
 
-const cors = require("cors");
-app.use(cors(corsOptions));
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
